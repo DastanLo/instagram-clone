4045812980716f3a9c643893c22728ca321e19b3
Dastan <dastansulaimankulov98@gmail.com>
2020-12-17 19:07:58 +0600

    removed url constant

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:5000';
+export const API_URL = '';
 export const defaultAvatar = 'https://res.cloudinary.com/dshzgegxx/image/upload/v1600712646/hs7fnysib8edzu0wcudo.png';
